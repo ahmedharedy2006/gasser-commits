d8fd0244040755112aca1f76eed54a589bc93979
ahassan200695 <ahassan200695@gmail.com>
2025-02-26 16:35:14 +0200

    first part

---
+++ 2nd commit/StateController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrendsValley.DataAccess.Data;
+using TrendsValley.Models.Models;
+
+namespace TrendsValley.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class StateController : Controller
+    {
+        private readonly AppDbContext _db;
+        public StateController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<State> objList = _db.states.ToList();
+            return View(objList);
+        }
+
+
+        public IActionResult Upsert(int? id)
+        {
+            State obj = new();
+            if (id == null || id == 0)
+            {
+                return View(obj);
+            }
+            obj = _db.states.FirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(State obj)
+        {
+            if (obj == null)
+            {
+                await _db.states.AddAsync(obj);
+            }
+            else
+            {
+                _db.states.Update(obj);
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            State obj = new();
+            obj = _db.states.FirstOrDefault(s => s.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.states.Remove(obj);
+            }
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ 5th commit/CityController.cs
@@ -0,0 +1,73 @@
+﻿using BooksMine.Models;
+using Microsoft.AspNetCore.Mvc;
+using TrendsValley.DataAccess.Data;
+using TrendsValley.Models.Models;
+
+namespace TrendsValley.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CityController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<City> objList = _db.cities.ToList();
+            return View(objList);
+        }
+
+
+        public IActionResult Upsert(int? id)
+        {
+            City obj = new();
+            if (id == null || id == 0)
+            {
+                return View(obj);
+            }
+            obj = _db.cities.FirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(City obj)
+        {
+            if (obj.Id == null)
+            {
+                await _db.cities.AddAsync(obj);
+            }
+            else
+            {
+                _db.cities.Update(obj);
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        //Remove
+        public IActionResult Delete(int id)
+        {
+            City obj =new();
+            obj = _db.cities.FirstOrDefault(u => u.Id == id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.cities.Remove(obj);
+            }
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
